f6024783363a28757d059a860c7081a0de5eb814
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-04-04 18:08:25 -0400

    add variables for pools of different characters and prompt asking user how many characters they want in password

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  function generatePassword(){
+    // Add pool of characters to use for password
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var specialChara = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    var numbers = "0123456789"
+    var pool = "";
+    var numberofCharas = parseInt(prompt("How many characters do you want your password to have?"));
+
+
+  }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
