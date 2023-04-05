513c94d09f10e8e0f2ed39ea0034593e99a6dbb8
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-04-05 15:53:10 -0400

    add images folder with screenshot of live site, update readme with text under description and usage section and include screenshot of live site with links to github and the live site, move around code in js

--- README.md
+++ README.md
@@ -3,10 +3,20 @@ Password Generator Project
 
 ## Description
 
+The purpose of this Generate Password project was to teach bootcamp students how to use javascript to make a website function. Students had to write code in js including functions, if statements and for loops to ask the user what types of characters they would like used in the generated password. Those inputs from the user would then be used to generate a random password according to the users instructions. 
+
 ## Installation
 N/A
 
 ## Usage
+
+This website has one purpose, and that is to generate a random password given the input parameters from the user. After the user clicks on the "Generate Password" button, the window will then prompt them to answer a few questions. Those questions include, how long they want their password to be, and whether or not they want uppercase, lowercase, numeric and special characters to be included into their password. After all the windows pop-ups are answered, a random password will be generated based on how the previous questions were answered.  
+
+<img src="./images/password-generator-site.png" width= 500px />
+
+<a href="https://github.com/elvislau74/Password-Generator">Click here for the GitHub Repository</a>
+
+<a href="https://elvislau74.github.io/Password-Generator/">Click here for the Live Site</a>
 
 ## Credits
 N/A
--- script.js
+++ script.js
@@ -17,48 +17,50 @@ function generatePassword(){
   // spread operator - pool.push(...)
   var numberofCharas = parseInt(prompt("How many characters do you want your password to have?"));
 
-if (!numberofCharas){
-  return window.alert("Password length must be provided as a number.");
-}
+  if (!numberofCharas){
+    return window.alert("Password length must be provided as a number.");
+  }
 
-//Click okay to confirm if you would like to include special characters
-var addSpecial = confirm("Would you like to include special characters?");
-if (addSpecial){
-  pool.push(specialCharaArray);
-}
-else {
-  return;
-}
-//Click okay to confirm if you would like to include numeric characters
-var addNumeric = confirm("Would you like to include numeric characters?");
-if (addNumeric){
-  pool.push(numbersArray);
-}
-else {
-  return;
-}
-//Click okay to confirm if you would like to include lowercase letters
-var addLower = confirm("Would you like to include lowercase characters?");
-if (addLower){
-  pool.push(lowerCaseArray);
-}
-else {
-  return;
-}
-//Click okay to confirm if you would like to include uppercase letters
-var addUpper = confirm("Would you like to include uppercase characters?");
-if (addUpper){
-  pool.push(upperCaseArray);
-}
-else {
-  return;
-}
+  //Click okay to confirm if you would like to include special characters
+  var addSpecial = confirm("Would you like to include special characters?");
+  if (addSpecial){
+    pool.concat(specialCharaArray);
+  }
+  else {
+    return;
+  }
+  //Click okay to confirm if you would like to include numeric characters
+  var addNumeric = confirm("Would you like to include numeric characters?");
+  if (addNumeric){
+    pool.concat(numbersArray);
+  }
+  else {
+    return;
+  }
+  //Click okay to confirm if you would like to include lowercase letters
+  var addLower = confirm("Would you like to include lowercase characters?");
+  if (addLower){
+    pool.concat(lowerCaseArray);
+  }
+  else {
+    return;
+  }
+  //Click okay to confirm if you would like to include uppercase letters
+  var addUpper = confirm("Would you like to include uppercase characters?");
+  if (addUpper){
+    pool.concat(upperCaseArray);
+  }
+  else {
+    return;
+  }
 
-for (var i = 0; i < numberofCharas.length; i++){
-  passwordGenerated.push(pool[math.floor(math.random()*pool.length)]);
-}
+  for (var i = 0; i < numberofCharas.length; i++){
+    passwordGenerated.push(pool[Math.floor(Math.random() * pool.length)]);
+  }
 
-return passwordGenerated;
+  // window.alert("Your password is " + passwordGenerated);
+
+  return passwordGenerated;
 }
 
 // Write password to the #password input
@@ -68,7 +70,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  
 
 }
 
